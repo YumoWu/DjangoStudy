8f982fdee05870be0517f9b5786896cb66cf5510
武雨末 <wuyumoxdf@126.com>
2019-05-08 12:01:06 +0800

    表单提交

--- HelloWordDjango/testdb.py
+++ HelloWordDjango/testdb.py
@@ -22,4 +22,45 @@ def testdb(request):
     # 获取单个对象
     response3 = Test.objects.get(id=1)
 
-    
+    # 限制返回的数据 相当于 SQL 中的OFFSET 0 LIMIT 2
+    response4 = Test.objects.order_by('name')[0:2]
+
+    # 数据排序
+    response5 = Test.objects.order_by("id")
+
+    # 上面的方法可以连锁使用
+    response6 = Test.objects.filter(name="runoob").order_by("id")
+
+    # 输出所有数据
+    for var in list:
+        response1 += var.name + " "
+    response = response1
+
+    # return HttpResponse("<p>" + response + "</p>")
+
+    # 修改其中一个id=1的name字段，再save，相当于SQL中的UPDATE
+    test1 = Test.objects.get(id=1)
+    test1.name = "Google"
+    test1.save()
+
+    # 另外一种方式
+    Test.objects.filter(id=1).update(name="Google")
+
+    # 修改所有的列
+    Test.objects.all().update(name="Google")
+
+    # return HttpResponse("<p>修改成功</p>")
+
+    # 删除id=1的数据
+    test1 = Test.objects.get(id=1)
+    test1.delete()
+
+    # 另外一种方式
+    Test.objects.filter(id=1).delete()
+
+    # 删除所有数据
+    Test.objects.all().delete()
+    return HttpResponse("<p>删除成功</p>")
+
+
+
--- HelloWordDjango/urls.py
+++ HelloWordDjango/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls import url
 from django.urls import path
 from . import testdb
+from . import search
+from . import search2
 
 from . import view
 
 urlpatterns = [
     # url(r'^$', view.hello),
     path('hello/', view.hello),
-    url(r'^testdb$', testdb.testdb)
+    url(r'^testdb$', testdb.testdb),
+    url(r'^search-form$', search.search_form),
+    url(r'^search$', search.search),
+    url(r'^search-post$', search2.search_post),
 ]
