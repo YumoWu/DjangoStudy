8b62a0b21c69c53f462abb7114d6d3221957ab1b
武雨末 <wuyumoxdf@126.com>
2019-05-15 14:11:15 +0800

    admin模块

--- HelloWordDjango/urls.py
+++ HelloWordDjango/urls.py
@@ -1,16 +1,18 @@
 from django.conf.urls import url
-from django.urls import path
+# from django.urls import path
 from . import testdb
 from . import search
 from . import search2
+from django.contrib import admin
 
 from . import view
 
 urlpatterns = [
     # url(r'^$', view.hello),
-    path('hello/', view.hello),
+    # path('hello/', view.hello),
     url(r'^testdb$', testdb.testdb),
     url(r'^search-form$', search.search_form),
     url(r'^search$', search.search),
     url(r'^search-post$', search2.search_post),
+    url(r'^admin/', admin.site.urls)
 ]
